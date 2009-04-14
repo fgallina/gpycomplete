7fe1d475d2b8b3cf258a87f7160f01d6e06c734f
Fabián Ezequiel Gallina <fgallina@caffeinegroup.com.ar>
2009-04-14 05:04:44 -0300

    added tests

---
+++ tests/test2.py
@@ -0,0 +1,32 @@
+class ClassName(object):
+    """prueba
+    """
+
+    def __init__(self, arg1, arg2):
+        """
+
+        Arguments:
+        - `arg1`:
+        - `arg2`:
+        """
+        self._arg1 = arg1
+        self._arg2 = arg2
+
+
+    def prueba(self, test, test2):
+        """
+
+        Arguments:
+        - `self`:
+        - `test`:
+        - `test2`:
+        - `kwargs*`:
+        - `kwargss**`:
+        """
+
+    def test(self):
+        pass
+
+
+calefactor = True
+calefon = True
