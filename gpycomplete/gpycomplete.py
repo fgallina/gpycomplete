a6dbfb5e32458c5168a983f4bbd82069bdb3837a
Fabián Ezequiel Gallina <fgallina@cuca.(none)>
2009-09-10 03:33:19 -0300

    enhanced contact information

--- gpycomplete/gpycomplete.py
+++ gpycomplete/gpycomplete.py
@@ -14,7 +14,7 @@
 # along with gpycomplete.  If not, see <http://www.gnu.org/licenses/>.
 
 # gpycomplete is written from scratch by Fabian Ezequiel Gallina
-# <fgallina at caffeinegroup dot com dot ar> but it is somehow based on
+# <fgallina at from-the-cloud dot com> but it is somehow based on
 # the original pycomplete package from the http://python-mode.sf.net.
 
 # gpycomplete allows inline completion and help for the python
